4a6bdcedf899c7819987b56da4342b56f1bbb7de
Rudakivska-Marina <rudakivska.marina@gmail.com>
2024-02-17 19:49:03 +0200

    hw-4

---
+++ js/task-1.js
@@ -0,0 +1,25 @@
+function isEnoughCapacity(products, containerSize) {
+  let fullQuantity = 0;
+  const productsQuantity = Object.values(products);
+  for (const fruit of productsQuantity) {
+    fullQuantity += fruit;
+  }
+  return fullQuantity <= containerSize;
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+
---
+++ js/task-2.js
@@ -0,0 +1,37 @@
+function calcAverageCalories(days) {
+    if (days.length === 0) return 0;
+    let sumCalories = 0;
+    for (const calc of days) {
+        sumCalories += calc.calories;
+    }
+    return sumCalories / days.length;
+}
+
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
